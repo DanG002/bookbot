434e5830852fafa7fda14db86981d2fa8fcb924a
DanG002 <DanG002@example.com>
2024-02-29 21:03:45 +0200

    Print the word count of frankenstein.txt.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def print_book():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        print(len(words))
+
+
+if __name__ == "__main__":
+    print_book()
